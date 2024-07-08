8fca0c8247f158a28b3f68945d9f601ee8a7ba28
mubashir369 <muhammedmubashir3693@gmail.com>
2024-07-08 17:37:41 +0530

    2354

--- StackNavigator.js
+++ StackNavigator.js
@@ -7,15 +7,13 @@ import { AntDesign } from "@expo/vector-icons";
 import { Entypo } from "@expo/vector-icons";
 import SavedScreen from "./screens/SavedScreen";
 import BookingScreen from "./screens/BookingScreen";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
 import ProfileScreen from "./screens/ProfileScreen";
 import { NavigationContainer } from "@react-navigation/native";
 
-
-
 const StackNavigator = () => {
   const Tab = createBottomTabNavigator();
-  const Stack = createNativeStackNavigator()
+  const Stack = createNativeStackNavigator();
   const BottomTabs = () => {
     return (
       <Tab.Navigator>
@@ -33,7 +31,7 @@ const StackNavigator = () => {
               ),
           }}
         />
-         <Tab.Screen
+        <Tab.Screen
           name="Saved"
           component={SavedScreen}
           options={{
@@ -42,13 +40,12 @@ const StackNavigator = () => {
             tabBarIcon: ({ focused }) =>
               focused ? (
                 <Entypo name="heart" size={24} color="#003580" />
-               
               ) : (
                 <AntDesign name="hearto" size={24} color="black" />
               ),
           }}
         />
-         <Tab.Screen
+        <Tab.Screen
           name="Booking"
           component={BookingScreen}
           options={{
@@ -58,11 +55,15 @@ const StackNavigator = () => {
               focused ? (
                 <Ionicons name="notifications" size={24} color="#003580" />
               ) : (
-                <Ionicons name="notifications-outline" size={24} color="black" />
+                <Ionicons
+                  name="notifications-outline"
+                  size={24}
+                  color="black"
+                />
               ),
           }}
         />
-         <Tab.Screen
+        <Tab.Screen
           name="Profile"
           component={ProfileScreen}
           options={{
@@ -70,9 +71,17 @@ const StackNavigator = () => {
             headerShown: false,
             tabBarIcon: ({ focused }) =>
               focused ? (
-                <Ionicons name="person-circle-sharp" size={24} color="#003580" />
+                <Ionicons
+                  name="person-circle-sharp"
+                  size={24}
+                  color="#003580"
+                />
               ) : (
-                <Ionicons name="person-circle-outline" size={24} color="black" />
+                <Ionicons
+                  name="person-circle-outline"
+                  size={24}
+                  color="black"
+                />
               ),
           }}
         />
@@ -82,7 +91,11 @@ const StackNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Main" component={BottomTabs} options={{headerShown:false}} />
+        <Stack.Screen
+          name="Main"
+          component={BottomTabs}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,33 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useLayoutEffect } from 'react'
-import { useNavigation } from '@react-navigation/native'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
-    const navigation=useNavigation()
-    useLayoutEffect(()=>{
-        navigation.setOptions({
-            headerShown:true,
-            title:"Booking.com",
-            headerTitleStyle:{
-                fontSize:20,
-                fontWeight:"bold",
-                color:"white"
-            },
-            headerStyle:{
-                backgroundColor:"#003580",
-                height:110,
-                
-            }
-        })
-
-    },[])
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      title: "Booking.com",
+      headerTitleStyle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "white",
+      },
+      headerStyle: {
+        backgroundColor: "#003580",
+        height: 110,
+      },
+    });
+  }, []);
   return (
     <View>
       <Text>HomeScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
