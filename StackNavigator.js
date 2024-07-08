458bf85a9b577b868e0c5a90511efd060fc88dc4
mubashir369 <muhammedmubashir3693@gmail.com>
2024-07-08 17:07:09 +0530

    started

---
+++ StackNavigator.js
@@ -0,0 +1,91 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import { AntDesign } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import SavedScreen from "./screens/SavedScreen";
+import BookingScreen from "./screens/BookingScreen";
+import { Ionicons } from '@expo/vector-icons';
+import ProfileScreen from "./screens/ProfileScreen";
+import { NavigationContainer } from "@react-navigation/native";
+
+
+
+const StackNavigator = () => {
+  const Tab = createBottomTabNavigator();
+  const Stack = createNativeStackNavigator()
+  const BottomTabs = () => {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "Home",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Entypo name="home" size={24} color="#003580" />
+              ) : (
+                <AntDesign name="home" size={24} color="black" />
+              ),
+          }}
+        />
+         <Tab.Screen
+          name="Saved"
+          component={SavedScreen}
+          options={{
+            tabBarLabel: "Saved",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Entypo name="heart" size={24} color="#003580" />
+               
+              ) : (
+                <AntDesign name="hearto" size={24} color="black" />
+              ),
+          }}
+        />
+         <Tab.Screen
+          name="Booking"
+          component={BookingScreen}
+          options={{
+            tabBarLabel: "Booking",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Ionicons name="notifications" size={24} color="#003580" />
+              ) : (
+                <Ionicons name="notifications-outline" size={24} color="black" />
+              ),
+          }}
+        />
+         <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Ionicons name="person-circle-sharp" size={24} color="#003580" />
+              ) : (
+                <Ionicons name="person-circle-outline" size={24} color="black" />
+              ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+  };
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={BottomTabs} options={{headerShown:false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default StackNavigator;
---
+++ screens/BookingScreen.js
@@ -0,0 +1,14 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class BookingScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>BookingScreen</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
